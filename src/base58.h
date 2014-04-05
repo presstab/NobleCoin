b49dfb723885b28fd5ed7cec7a986c4170cf8ca6
jlcurby <info@jasonlcurby.com>
2014-04-06 00:22:40 +1100

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Litecoin addresses start with L
+        PUBKEY_ADDRESS = 21, // NobleCoin addresses start with 9
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
